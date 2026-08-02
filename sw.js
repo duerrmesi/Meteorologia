3ebf33d0925cacddfe691a686301a838dae94f5d
duerrmesi <duerrmesi@gmail.com>
2026-08-02 18:04:44 +0200

    Increment CACHE_NAME to 'wetter-dashboard-v11'
    Updated CACHE_NAME to version 11 for caching.

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Service Worker Cache-Name für die Progressive Web App
 // Bei jeder Änderung an den gecachten Dateien die Versionsnummer erhöhen!
-const CACHE_NAME = 'wetter-dashboard-v10';
+const CACHE_NAME = 'wetter-dashboard-v11';
 
 
 // Liste aller Dateien, die lokal auf dem Smartphone zwischengespeichert werden sollen
